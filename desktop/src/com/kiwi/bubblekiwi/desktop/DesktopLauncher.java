06fb1e7208e58be3d7b1dbfbabbea32e2c23f79a
ostojan <ostojan@outlook.com>
2017-03-26 19:13:43 +0200

    Initial commit

---
+++ desktop/src/com/kiwi/bubblekiwi/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kiwi.bubblekiwi.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kiwi.bubblekiwi.BubbleKiwiGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BubbleKiwiGame(), config);
+	}
+}
