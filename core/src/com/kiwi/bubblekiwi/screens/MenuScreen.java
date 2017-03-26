c788bf50993951e9001480c064af212f025d63fb
ostojan <ostojan@outlook.com>
2017-03-26 20:37:03 +0200

    Empty menu screen

---
+++ core/src/com/kiwi/bubblekiwi/screens/MenuScreen.java
@@ -0,0 +1,14 @@
+package com.kiwi.bubblekiwi.screens;
+
+import com.kiwi.bubblekiwi.BubbleKiwiGame;
+
+public class MenuScreen extends AbstractScreen {
+    public MenuScreen(BubbleKiwiGame game) {
+        super(game);
+    }
+
+    @Override
+    protected void initialize() {
+
+    }
+}
