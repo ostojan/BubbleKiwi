47c537f8108da2270474f15a554015304fdf47a5
ostojan <ostojan@outlook.com>
2017-03-28 22:10:43 +0200

    Primitive player movement system

--- core/src/com/kiwi/bubblekiwi/entities/Player.java
+++ core/src/com/kiwi/bubblekiwi/entities/Player.java
@@ -7,11 +7,79 @@ import com.kiwi.bubblekiwi.BubbleKiwiGame;
 public class Player extends Image {
     private static final int WIDTH = 120;
     private static final int HEIGHT = 157;
+    private static final float MOVE_SPEED = 250.0f;
+    private static final float JUMP_SPEED = 500.0f;
+    private static final float FALL_SPEED = 520.0f;
+
+    private enum State {
+        STAYING,
+        MOVING_LEFT,
+        MOVING_RIGHT,
+        JUMPING,
+        FALLING
+        }
+    private State state;
 
     public Player() {
         super(new Texture("kiwi.png"));
         setSize(WIDTH, HEIGHT);
         setOrigin(WIDTH / 2.0f, HEIGHT / 2.0f);
         setPosition((BubbleKiwiGame.WIDTH - WIDTH) / 2.0f, 20);
+        state = State.STAYING;
+    }
+
+    public void moveRight() {
+        if (state == State.STAYING) {
+            state = State.MOVING_RIGHT;
+        }
+    }
+
+    public void moveLeft() {
+        if (state == State.STAYING) {
+            state = State.MOVING_LEFT;
+        }
+    }
+
+    public void jump() {
+        if (state == State.STAYING) {
+            state = State.JUMPING;
+        }
+    }
+
+    public void stopMoving() {
+        if (!isInAir()) {
+            state = State.STAYING;
+        }
+    }
+
+    private boolean isInAir() {
+        return state == State.FALLING || state == State.JUMPING;
+    }
+
+    public void update(float delta) {
+        switch (state) {
+            case MOVING_LEFT:
+                if (getX() > 0) {
+                    moveBy(-delta * MOVE_SPEED, 0);
+                }
+                break;
+            case MOVING_RIGHT:
+                if (getX() < BubbleKiwiGame.WIDTH - WIDTH) {
+                    moveBy(delta * MOVE_SPEED, 0);
+                }
+                break;
+            case FALLING:
+                if (getY() <= 20) {
+                    state = State.STAYING;
+                }
+                moveBy(0, -delta * FALL_SPEED);
+                break;
+            case JUMPING:
+                if (getY() >= 180) {
+                    state = State.FALLING;
+                }
+                moveBy(0, delta * JUMP_SPEED);
+                break;
+        }
     }
 }
--- core/src/com/kiwi/bubblekiwi/screens/GameplayScreen.java
+++ core/src/com/kiwi/bubblekiwi/screens/GameplayScreen.java
@@ -2,14 +2,22 @@ package com.kiwi.bubblekiwi.screens;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.kiwi.bubblekiwi.BubbleKiwiGame;
 import com.kiwi.bubblekiwi.controllers.BubblesController;
 import com.kiwi.bubblekiwi.entities.ParallaxBackground;
 import com.kiwi.bubblekiwi.entities.ParallaxLayer;
+import com.kiwi.bubblekiwi.entities.Player;
 
 public class GameplayScreen extends AbstractScreen {
     private ParallaxBackground background;
+    private Player player;
     private BubblesController bubblesController;
+    private Button leftButton;
+    private Button rightButton;
+    private Button jumpButton;
 
     public GameplayScreen(BubbleKiwiGame game) {
         super(game);
@@ -19,7 +27,9 @@ public class GameplayScreen extends AbstractScreen {
     protected void initialize() {
         backgroundColor = Color.BLACK;
         initializeBackground();
+        initializePlayer();
         initializeBubblesController();
+        initializePlayerControlButtons();
     }
 
     private void initializeBackground() {
@@ -29,9 +39,58 @@ public class GameplayScreen extends AbstractScreen {
         stage.addActor(background);
     }
 
+    private void initializePlayer() {
+        player = new Player();
+        stage.addActor(player);
+    }
+
     private void initializeBubblesController() {
         bubblesController = new BubblesController();
         stage.addActor(bubblesController);
+    }
+
+    private void initializePlayerControlButtons() {
+        initializeLeftControlButton();
+        initializeRightControlButton();
+        initializeJumpControlButton();
+    }
+
+    private void initializeLeftControlButton() {
+        leftButton = new Button(new Button.ButtonStyle());
+        leftButton.setSize(120, 120);
+        leftButton.setPosition(20, 20);
+        leftButton.setDebug(true);
+        leftButton.addListener(new ClickListener() {
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                super.touchUp(event, x, y, pointer, button);
+                player.stopMoving();
+            }
+        });
+        stage.addActor(leftButton);
+    }
+
+    private void initializeRightControlButton() {
+        rightButton = new Button(new Button.ButtonStyle());
+        rightButton.setSize(120, 120);
+        rightButton.setPosition(160, 20);
+        rightButton.setDebug(true);
+        rightButton.addListener(new ClickListener() {
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                super.touchUp(event, x, y, pointer, button);
+                player.stopMoving();
+            }
+        });
+        stage.addActor(rightButton);
+    }
+
+    private void initializeJumpControlButton() {
+        jumpButton = new Button(new Button.ButtonStyle());
+        jumpButton.setSize(120, 120);
+        jumpButton.setPosition(BubbleKiwiGame.WIDTH - 140, 20);
+        jumpButton.setDebug(true);
+        stage.addActor(jumpButton);
     }
 
     @Override
@@ -47,6 +106,16 @@ public class GameplayScreen extends AbstractScreen {
 
     private void update(float delta) {
         background.update(delta);
+        if (leftButton.isPressed()) {
+            player.moveLeft();
+        }
+        if (rightButton.isPressed()) {
+            player.moveRight();
+        }
+        if (jumpButton.isPressed()) {
+            player.jump();
+        }
+        player.update(delta);
         bubblesController.update(delta);
     }
 }
