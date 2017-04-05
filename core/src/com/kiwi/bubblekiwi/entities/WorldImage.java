c4840f2ce48db446874256013b5511b7efa76a8f
ostojan <ostojan@outlook.com>
2017-04-05 19:52:26 +0200

    WorldImage abstract class

---
+++ core/src/com/kiwi/bubblekiwi/entities/WorldImage.java
@@ -0,0 +1,21 @@
+package com.kiwi.bubblekiwi.entities;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.kiwi.bubblekiwi.BubbleKiwiGame;
+
+public class WorldImage extends Image {
+    @Override
+    public void setOrigin(float originX, float originY) {
+        super.setOrigin(originX / BubbleKiwiGame.PPM, originY / BubbleKiwiGame.PPM);
+    }
+
+    @Override
+    public void setSize(float width, float height) {
+        super.setSize(width / BubbleKiwiGame.PPM, height / BubbleKiwiGame.PPM);
+    }
+
+    @Override
+    public void setPosition(float x, float y) {
+        super.setPosition(x / BubbleKiwiGame.PPM, y / BubbleKiwiGame.PPM);
+    }
+}
